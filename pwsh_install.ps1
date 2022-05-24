4e833b886db52347c6f26a8829ca80643751521c
Nguyễn Duy Thắng <duythang.2912@kotonosora.com>
2022-05-25 03:20:18 +0700

    improve config

---
+++ pwsh_install.ps1
@@ -0,0 +1,16 @@
+# Install module pwsh
+# Install-Module -Name posh-git -Repository PSGallery -Force
+# Install-Module -Name PSFzf -Repository PSGallery -Force
+# Install-Module -Name PSReadLine -Repository PSGallery -Force
+# Install-Module -Name z -Repository PSGallery -Force
+# Install-Module -Name Terminal-Icons -Repository PSGallery -Force
+
+# Install oh-my-posh
+# https://ohmyposh.dev/docs/installation/windows
+# scoop install https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/oh-my-posh.json
+scoop status
+# scoop list
+
+# Get all module installed and update
+# Get-InstalledModule | Update-Module
+Get-InstalledModule
